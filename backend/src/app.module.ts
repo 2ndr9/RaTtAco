54011353bd11647f13a96b8d6088e58757be128e
2ndr9 <rk17tnd@gmail.com>
2021-09-07 12:41:14 +0900

    update backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { User } from './users/users.entity';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'rattaco-local',
+      password: 'rattaco-pass',
+      database: 'test',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
