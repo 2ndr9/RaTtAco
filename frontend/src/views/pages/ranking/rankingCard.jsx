e537be94798563a15d6d1b41a73411ae6895ba7b
kimnigang <zeus2424797@gmail.com>
2021-09-07 16:12:48 +0900

    ranking second commit

--- frontend/src/views/pages/ranking/rankingCard.jsx
+++ frontend/src/views/pages/ranking/rankingCard.jsx
@@ -3,10 +3,11 @@ import React from "react";
 export const RankingCard = () => {
   return (
     <div className="CardContainer">
-      <div className="CardNumber">1</div>
-      <div className="CardName">KANG</div>
-
-      <div className="CardTime">0:15</div>
+      <div>
+        <div className="CardNumber">1</div>
+        <div className="CardTime">0:15</div>
+      </div>
+      <div className="CardName"> KANG</div>
     </div>
   );
 };
