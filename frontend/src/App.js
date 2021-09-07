ca7c83459e4af75895215fbd00914d14a01d8d44
mhousetree <mhousetree@gmail.com>
2021-09-07 17:12:56 +0900

    adopted web font(wip)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Helmet } from "react-helmet";
 import top from "./views/pages/top/top";
 import record from "./views/pages/record/record";
 import ranking from "./views/pages/ranking/ranking.jsx";
@@ -10,12 +11,26 @@ class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Helmet>
+          <script>
+            {`
+          (function(d) {
+            var config = {
+              kitId: 'nkn0nbx',
+              scriptTimeout: 3000,
+              async: true
+            },
+            h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+          })(document);
+        `}
+          </script>
+        </Helmet>
         <Header />
         <main>
           <Switch>
-          <Route exact path="/" component={top} />
-          <Route exact path="/record" component={record} />
-          <Route exact path="/ranking" component={ranking} />
+            <Route exact path="/" component={top} />
+            <Route exact path="/record" component={record} />
+            <Route exact path="/ranking" component={ranking} />
           </Switch>
         </main>
       </BrowserRouter>
