b0e8d996366444e617ccefe63d2a3d4d6ae26f3b
mhousetree <mhousetree@gmail.com>
2021-09-08 23:23:31 +0900

    add selects in create-page

--- frontend/src/views/pages/create/create.js
+++ frontend/src/views/pages/create/create.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Select from "react-select";
 import SubpageHead from "../../components/SubpageHead";
-import CategoryForm from "./categoryForm";
+import TagSelect from "./tagSelect";
+import options from "../../components/categoryTags";
 
 import "../../components/forms.scss";
 import "./create.scss";
@@ -10,28 +12,28 @@ class create extends React.Component {
     return (
       <div id="create">
         <SubpageHead title="種目登録" name="create" />
-        <p>Create New Event</p>
         <form
           method="POST"
           action="/auth/create"
-          class="one-to-one_form"
-          className="eventForm"
+          className="one-to-one_form eventForm"
         >
-          <label for="name" class="required">
-            種目名&nbsp;
-            <span className={"redMust"}>必須</span>
+          <label for="name" className="required">
+            種目名
           </label>
           <input type="text" name="name" required></input>
-          <label for="bio" class="required">
-            種目の説明&nbsp;
-            <span className={"redMust"}>必須</span>
+          <label for="bio" className="required">
+            種目の説明
           </label>
-          <textarea type="text" name="bio"></textarea>
-          <CategoryForm />
+          <textarea type="text" name="bio" required></textarea>
+          <label for="category_tag" className="required">
+            種目タグ
+          </label>
+          <Select options={options} placeholder="" />
+          <TagSelect />
           <button
             type="submit"
             id={"createButton"}
-            class="button-wide-red button-register"
+            className="button-wide-red button-register"
           >
             新規登録
           </button>
