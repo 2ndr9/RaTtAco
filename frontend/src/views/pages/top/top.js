97660c13e7c60a626cfbc511822a0e3c6fd8924b
kimnigang <zeus2424797@gmail.com>
2021-09-08 17:34:09 +0900

    first commit

--- frontend/src/views/pages/top/top.js
+++ frontend/src/views/pages/top/top.js
@@ -1,9 +1,95 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Autosuggest from "react-autosuggest";
 import "./top.scss";
 
+const languages = [
+  {
+    name: "掃除",
+  },
+  {
+    name: "洗濯",
+  },
+  {
+    name: "散歩",
+  },
+  {
+    name: "皿洗い",
+  },
+  {
+    name: "焼きそば作り",
+  },
+  //...
+];
+
+function escapeRegexCharacters(str) {
+  return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+}
+
+function getSuggestions(value) {
+  const escapedValue = escapeRegexCharacters(value.trim());
+
+  if (escapedValue === "") {
+    return [];
+  }
+
+  const regex = new RegExp("^" + escapedValue, "i");
+
+  return languages.filter((language) => regex.test(language.name));
+}
+
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
+
+function renderSuggestion(suggestion) {
+  return (
+    <div>
+      <Link to="/tasks" id="totasks" className={"suggestionContainer"}>
+        <span>{suggestion.name}</span>
+      </Link>
+    </div>
+  );
+}
+
+const renderInputComponent = (inputProps) => (
+  <div>
+    <input id="city-name-suggestion" className="form-control" {...inputProps} />
+  </div>
+);
+
 class top extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      value: "",
+      suggestions: [],
+    };
+  }
+
+  onChange = (event, { newValue, method }) => {
+    this.setState({ value: newValue });
+  };
+
+  onSuggestionsFetchRequested = ({ value }) => {
+    this.setState({ suggestions: getSuggestions(value) });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({ suggestions: [] });
+  };
+
   render() {
+    const { value, suggestions } = this.state;
+
+    const inputProps = {
+      placeholder: "search",
+      value,
+      onChange: this.onChange,
+      className: "form-control form-control-lg",
+    };
+
     return (
       <div>
         <section id="eyecatch" className="full-width">
@@ -25,10 +111,20 @@ class top extends React.Component {
           </p>
           <form method="POST" action="/">
             <p>
-              <input type="text" />
+              {/* <input type="text" />
               <button type="submit" id="search-button">
                 <i class="fas fa-search"></i>
-              </button>
+              </button> */}
+
+              <Autosuggest
+                suggestions={suggestions}
+                onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+                onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+                getSuggestionValue={getSuggestionValue}
+                renderSuggestion={renderSuggestion}
+                renderInputComponent={renderInputComponent}
+                inputProps={inputProps}
+              />
             </p>
           </form>
 
