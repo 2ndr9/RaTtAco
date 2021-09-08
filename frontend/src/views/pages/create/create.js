98a97106869937c066f8acb47f215730b7d4dca1
kimnigang <zeus2424797@gmail.com>
2021-09-08 21:08:34 +0900

    second commit

--- frontend/src/views/pages/create/create.js
+++ frontend/src/views/pages/create/create.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SubpageHead from "../../components/SubpageHead";
+import CategoryForm from "./categoryForm";
 
 import "../../components/forms.scss";
 import "./create.scss";
@@ -7,10 +8,15 @@ import "./create.scss";
 class create extends React.Component {
   render() {
     return (
-      <div id="register">
-        <SubpageHead title="種目登録" name="register" />
+      <div id="create">
+        <SubpageHead title="種目登録" name="create" />
         <p>Create New Event</p>
-        <form method="POST" action="/auth/register" class="one-to-one_form">
+        <form
+          method="POST"
+          action="/auth/create"
+          class="one-to-one_form"
+          className="eventForm"
+        >
           <label for="name" class="required">
             種目名&nbsp;
             <span className={"redMust"}>必須</span>
@@ -21,7 +27,12 @@ class create extends React.Component {
             <span className={"redMust"}>必須</span>
           </label>
           <textarea type="text" name="bio"></textarea>
-          <button type="submit" class="button-wide-red button-register">
+          <CategoryForm />
+          <button
+            type="submit"
+            id={"createButton"}
+            class="button-wide-red button-register"
+          >
             新規登録
           </button>
         </form>
