1d5dce41238c5b84e4957b9de48f5e0b5c0edda8
2ndr9 <rk17tnd@gmail.com>
2022-06-10 21:07:39 +0900

    up

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,6 @@ async function bootstrap() {
 
   app.enableCors({
     origin: `${process.env.CORS_ALLOW_ORIGIN}`,
-    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   });
 
